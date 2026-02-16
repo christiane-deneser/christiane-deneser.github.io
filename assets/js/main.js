d5c112035307dd902a58eddc8254cbbc6c023aa5
Christiane <mail@christianedeneser.com>
2026-02-16 12:59:29 +0100

    add footer to services page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
+/**
+ * JAVASCRIPT
+ * */
+import Header from "./modules/header.js";
+
+const headers = document.querySelectorAll(".js-header");
+headers.forEach((el) => {
+  new Header(el);
+});
+
+
 // Shared JS (global)
 // assets/js/main.js for site-wide enhancements (nav, theme toggle later, etc.)
 // assets/js/modules/* for reusable utilities.
